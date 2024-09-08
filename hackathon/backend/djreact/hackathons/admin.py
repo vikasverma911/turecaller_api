60ff9a232779dc230fdeb0fa5f38e8cd7feaf33e
Vikas <vikas911.verma@gmail.com>
2024-09-08 18:14:44 +0530

    Remove

--- hackathon/backend/djreact/hackathons/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Hackathon, HackathonSubmission
-
-admin.site.register(Hackathon)
-admin.site.register(HackathonSubmission)
-# Register your models here.
